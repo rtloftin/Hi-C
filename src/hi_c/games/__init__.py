775e469cb94d713b574d5c7700dfa5c59d8e04d5
Robert Loftin <rloftin4400@gmail.com>
2023-03-16 06:57:08 +0100

    added experiments for the quadratic optimization task

--- src/hi_c/games/__init__.py
+++ src/hi_c/games/__init__.py
@@ -1,11 +1,14 @@
 from hi_c.games.matrix import IteratedGame, MatrixGame
 from hi_c.games.polynomial import TandemGame, HamiltonianGame
+from hi_c.games.optimization import Quadratic, Gaussian
 
 GAMES = {
     "iterated": IteratedGame,
     "matrix": MatrixGame,
     "tandem": TandemGame,
     "hamiltonian": HamiltonianGame,
+    "quadratic": Quadratic,
+    "gaussian": Gaussian,
 }
 
 def get_game_class(name):
